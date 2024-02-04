408a707336775396844c6884ecb20895cd56ee89
Renato Nazzari <renatomlnz@gmail.com>
2024-02-04 14:35:43 -0300

    Finalizando o projeto Alura Midi

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,5 +1,11 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        alert('Elemento não encontrado');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -16,4 +22,18 @@ for (let i = 0; i < listaDeTeclas.length; i++) {
         tocaSom(`#som_${instrumento}`);
     };
 
-}; 
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code);
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        };
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+};
+
